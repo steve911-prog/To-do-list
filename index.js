1392c209166da854298aaf048c272c516347b911
steve911-prog <bwlad0911@gmail.com>
2023-07-06 17:39:22 +0200

    1 sprint
    Navbar, table started

---
+++ index.js
@@ -0,0 +1,19 @@
+newTask = document.getElementById("newTask");
+newTextInput = document.getElementsByClassName("new");
+
+
+
+// window.addEventListener('load', function(){
+//     newTextInput.click = () => {
+//       console.log("listener is added");
+//       newTextInput.style = "display:flex;";
+//     };
+
+// });
+
+// function showInput(){
+//     console.log("listener is added");
+//     newTextInput = document.getElementsByClassName("new");
+//     console.log(newTextInput.style);
+// };
+
