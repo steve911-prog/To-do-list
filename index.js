a083ae844447da5cedeb3b2d3cd79d4996d8fc1b
steve911-prog <bwlad0911@gmail.com>
2023-07-07 16:59:18 +0200

    Update

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-newTask = document.getElementById("newTask");
-newTextInput = document.getElementsByClassName("new");
-
-newTask.addEventListener("click", function () {
-  newTextInput.click = () => {
-    console.log("listener is added");
-    newTextInput.style = "display:flex;";
+function addTask() {
+  let title = document.querySelector("#taskTitle");
+  let tasks = document.querySelector(".tasks");
+  console.log(title.value);
+  if(title.value === ''){
+    return;
   };
-});
+  let task = document.createElement("div");
+  task.setAttribute("class", `task gray ${tasks.childElementCount + 1}`);
+  task.innerHTML = ` <div class="check" ><i class="fa-solid fa-check fa-xl"></i></div>
+            <div class="label"><h2 class="label">NEW TASK</h2></div>
+            <div class="delete"><i class="fa-solid fa-xmark"></i></i></div>`;
+  tasks.appendChild(task);
+}
 
-function showInput() {
-  console.log("listener is added");
-  newTextInput = document.getElementsByClassName("new");
-  
-  console.log(newTextInput.textContent);
-};
+function doneTask(num, event) {
+  console.log("Clicked element: ", event);
+
+  let check = document.querySelector(`.${num} .check`);
+  console.log(check.textContent);
+  let checkI = document.querySelector(`.${num} .check i`);
+
+  if (checkI.style.display === "block") {
+    checkI.style.display = "none";
+  } else {
+    checkI.style.display = "block";
+  }
+}
