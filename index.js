ae2054d4a98a200b638bd23177a0efd5d09af816
steve911-prog <bwlad0911@gmail.com>
2023-07-18 14:14:51 +0200

    Yesterday Update

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
+const numbersToWords = {
+  0: "zero",
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  11: "eleven",
+  12: "twelve",
+  13: "thirteen",
+  14: "fourteen",
+  15: "fifteen",
+  16: "sixteen",
+  17: "seventeen",
+  18: "eighteen",
+  19: "nineteen",
+  20: "twenty",
+  30: "thirty",
+  40: "forty",
+  50: "fifty",
+  60: "sixty",
+  70: "seventy",
+  80: "eighty",
+  90: "ninety",
+};
+
 function addTask() {
   let title = document.querySelector("#taskTitle");
   let tasks = document.querySelector(".tasks");
   console.log(title.value);
-  if(title.value === ''){
+  if (title.value === "") {
     return;
-  };
+  }
   let task = document.createElement("div");
-  task.setAttribute("class", `task gray ${tasks.childElementCount + 1}`);
-  task.innerHTML = ` <div class="check" ><i class="fa-solid fa-check fa-xl"></i></div>
-            <div class="label"><h2 class="label">NEW TASK</h2></div>
-            <div class="delete"><i class="fa-solid fa-xmark"></i></i></div>`;
+  task.setAttribute("class", `task gray ${numbersToWords[tasks.childElementCount + 1]}`);
+  task.innerHTML = ` <div class="check" onclick = 'doneTask("${
+    numbersToWords[tasks.childElementCount + 1]
+  }")'><i class="fa-solid fa-check fa-xl"></i></div>
+            <div class="label"><h2 class="label">${title.value}</h2></div>
+            <div class="delete" onclick="deleteTask('${numbersToWords[tasks.childElementCount + 1]}')"><i class="fa-solid fa-xmark"></i></i></div>`;
+  
+  console.log(tasks.childElementCount);
   tasks.appendChild(task);
+  console.log(tasks.childElementCount);
 
-  title.value = '';
+  title.value = "";
+  
 }
 
-function doneTask(num, event) {
-  console.log("Clicked element: ", event);
-
+function doneTask(num) {
+  // console.log("Clicked element: ");
+  let tasks = document.querySelector(".tasks");
   let check = document.querySelector(`.${num} .check`);
-  console.log(check.textContent);
+  // console.log(check.textContent);
   let checkI = document.querySelector(`.${num} .check i`);
 
   if (checkI.style.display === "block") {
@@ -27,4 +64,33 @@ function doneTask(num, event) {
   } else {
     checkI.style.display = "block";
   }
+
+  // for(let i = 0; i < tasks.childNodes; i++){
+  //     console.log(tasks.childNodes[i]);
+  //     let elem = document.querySelector(`${tasks.childNodes[i]} .check i`);
+
+  //     console.log(elem.innerHTML);
+  //     let doneTasks = 0;
+  //     if(elem.style.display === "block"){
+  //       doneTasks++;
+  //       if(tasks.childElementCount == doneTasks){
+  //           console.log("Congratulations! You have done everything!");
+  //       }
+  //     }
+  // };
+
+
+
+
+};
+
+function deleteTask(num){
+  console.log('Function is working :)' , num);
+  let tasks = document.querySelector(".tasks");
+  let child = document.querySelector(`.${num}`);
+
+  tasks.removeChild(child);
+  console.log(tasks.childElementCount);
+
+
 }
